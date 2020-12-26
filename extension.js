4393dc16bb7380993bd09ecd03903e0458bfa161
JINAVAMSA <jinavamsa@MacBook-Pro-cua-JinaSasana.local>
2020-12-26 09:29:13 +0700

    up

--- extension.js
+++ extension.js
@@ -3,7 +3,8 @@ const {
 } = require('playwright');
 
 (async () => {
-  const pathToExtension = require('path').join(__dirname, 'metamask-chrome-8.1.3');
+  const pathToExtension = require('path').join(__dirname, './utils/browser/metamask-chrome-8.1.3');
+  console.log('pathToExtension', pathToExtension)
   const userDataDir = '/tmp/test-user-data-dir';
   const browserContext = await chromium.launchPersistentContext(userDataDir, {
     headless: false,
@@ -34,7 +35,7 @@ const {
   await metamaskPage.click('text="Goerli Test Network"')
   
   const page = await browserContext.newPage()
-  await page.goto('https://zed-front-pr-333.herokuapp.com', { timeout: 0 });
+  await page.goto('https://goerli-test.zed.run/', { timeout: 0 });
 
   // await metamaskPage.bringToFront();
 
@@ -62,20 +63,21 @@ const {
   await metaMaskSign2.waitForEvent("close")
 
   // buy horst
-  await page.click('text="Marketplace"')
-  await page.click('.horse-sale-card')
-  await page.click('text="Buy with ETH"')
-  await page.waitForTimeout(2000)
+  // await page.click('text="Marketplace"')
+  // await page.click('.horse-sale-card')
+  // await page.click('text="Buy with ETH"')
+  // await page.waitForTimeout(2000)
   // const [popup] = await Promise.all([
   //   page.waitForEvent('popup'),
   //   page.click('text="Buy with ETH"')
   // ])
   // const check = await page.$('text="confirm"')
   // console.log('check:', check)
-  await page.hover('.confirm-btn')
-  await page.dblclick('.confirm-btn', { button: "left", force: true })
+  // await page.hover('.confirm-btn')
+  // await page.dblclick('.confirm-btn', { button: "left", force: true })
 
   // Test the background page as you would any other page.
-  // await browserContext.close();
+  console.log('success')
+  await browserContext.close();
 
 })();
