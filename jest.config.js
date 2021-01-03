322616183a3d206dc307a07f09c82219a346372f
davidpropfin <letuananh.it1405@gmail.com>
2021-01-03 17:34:17 +0700

    jest time out

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     setupFilesAfterEnv: ['jest-allure/dist/setup'],
-    testMatch: ["**/tests/*.test.js", "**/tests/**/*.test.js"]
+    testMatch: ["**/tests/*.test.js", "**/tests/**/*.test.js"],
+    testTimeout: 10000
 };
