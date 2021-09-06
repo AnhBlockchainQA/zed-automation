cd5bbd6a58527aabd5a2444fb4a66bdcfa90559f
Joshua Briceno Z <j3group.s.a@gmail.com>
2021-09-06 10:41:15 -0600

    Correcting Stage on GitHub Actions.

--- jest-playwright.config.js
+++ jest-playwright.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     browser: ["chromium"],
+    contextOptions: {
+        ignoreHTTPSErrors: true,
+        viewport: {
+            width: 1280,
+            height: 800
+        }
+    },
     errorOnPageError: false,
     launchOptions:{
         headless: false
