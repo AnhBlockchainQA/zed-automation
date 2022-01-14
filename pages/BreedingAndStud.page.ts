f4a902d5d827716d7507012e34829c4626d2ab06
ratanvhs <95623261+ratanvhs@users.noreply.github.com>
2022-01-14 16:24:03 +0530

    APAC-2943 / ZED-212
    ZED-212 - Breeding Horse Details Panel shown the Horse OWNER STABLE

--- pages/BreedingAndStud.page.ts
+++ pages/BreedingAndStud.page.ts
@@ -65,6 +65,7 @@ class BreedingAndStud {
       txtStudFeeBox: (id: number) => `(//div[@class='price-input']//div)[${id}]`,
       lblZedGen: (id: number) => `(//div[@class='properties-content'])[${id}]/div[1]/span[1]`,
       lblBreedType: (id: number) => `(//div[@class='properties-content'])[${id}]/div[1]/span[2]`,
+      lblStableOwner: (id: number) => `(//div[contains(@class,'primary-text green')])[${id}]`,
     },
     filtersPanel: {
       divPanelFilterStud: '//div[contains(@class,"sidebar-wrapper side-filter-wrapper")]',
