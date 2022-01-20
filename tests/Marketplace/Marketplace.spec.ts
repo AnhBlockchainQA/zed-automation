2b89dd85875ee30f1b1eaf0ba6b00e560fd472a2
Uthpala Jayasinghe <97392767+ujayasinghe@users.noreply.github.com>
2022-01-20 14:21:05 +0530

    OA-726/ZED-8
    Added Tests for Marketplace is showing the Horse List at Sale

--- tests/Marketplace/Marketplace.spec.ts
+++ tests/Marketplace/Marketplace.spec.ts
@@ -1,6 +1,7 @@
 import Authorization from '../../pages/Authorization.page';
 import * as data from '../../fixtures/qa.json';
 import Metamask from '../../pages/Metamask.module';
+import Marketplace from '../../pages/Marketplace.page';
 import { BrowserContext } from 'playwright';
 
 describe('Marketplace', () => {
@@ -8,17 +9,21 @@ describe('Marketplace', () => {
   let pages: any;
   let browserContext: BrowserContext;
   let metamask: Metamask;
+  let marketplace: Marketplace;
 
   beforeAll(async () => {
     metamask = new Metamask();
     browserContext = await metamask.init();
     pages = await metamask.authenticate(browserContext);
     auth = new Authorization(pages);
+    marketplace = new Marketplace(pages);
   });
 
   beforeEach(async () => {
     await pages[0].goto(data.baseUrl);
     await pages[0].waitForLoadState();
+    await pages[0].waitForTimeout(3000);
+    await pages[0].click(marketplace.objects.btnMarketPlace);
   });
 
   afterAll(async () => {
@@ -27,8 +32,17 @@ describe('Marketplace', () => {
     await metamask.close(pages, browserContext);
   });
 
-  xit('ZED-XX - Not Implemented Yet', async () => {
-    expect(await pages[0].isVisible(auth.objects.B_ETH_BALANCE)).toBe(true);
+  it('ZED-8 - Marketplace is showing the Horse List at Sale', async () => {
+    await pages[0].waitForSelector(marketplace.objects.firsthorseCard)
+    await pages[0].waitForTimeout(3000);
+    expect(await pages[0].isVisible(marketplace.objects.marketPlaceFilter)).toBe(true);
+    expect(await pages[0].isVisible(marketplace.objects.marketPlaceContent)).toBe(true);
+    expect(await pages[0].isVisible(marketplace.objects.horseDetailsCard)).toBe(true);
+    await pages[0].click(marketplace.objects.firsthorseCard);
+    await pages[0].waitForSelector(marketplace.objects.buyButton)
+    expect(await pages[0].isVisible(marketplace.objects.buyButton)).toBe(true);
+    expect(await pages[0].isVisible(marketplace.objects.priceBadge)).toBe(true);
+    expect(await pages[0].innerText(marketplace.objects.buyButton)).toContain(data.buy_btn_text);
   });
 
 });
