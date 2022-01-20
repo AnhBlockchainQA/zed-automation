095bd7d1868a7e844fa11d5acb53c5241da0ad2a
ratanvhs <95623261+ratanvhs@users.noreply.github.com>
2022-01-20 16:46:59 +0530

    APAC-2941 / ZED-210
    ZED-210 - Breeding Horse Details Panel shown the Horse WIN RATE

--- pages/BreedingAndStud.page.ts
+++ pages/BreedingAndStud.page.ts
@@ -77,6 +77,7 @@ class BreedingAndStud {
       lblBloodline: (id: number) => `(//div[@class='properties-content'])[${id}]/div[2]/div[2]`,
       lblStableOwner: (id: number) => `(//div[contains(@class,'primary-text green')])[${id}]`,
       lblCareerValue: (id: number) => `(//div[@class='properties-content'])[${id}]/div[6]/div[2]`,
+      lblWinRateValue: (id: number) => `(//div[@class='properties-content'])[${id}]/div[7]/div[2]`,
     },
     filtersPanel: {
       divPanelFilterStud: '//div[contains(@class,"sidebar-wrapper side-filter-wrapper")]',
