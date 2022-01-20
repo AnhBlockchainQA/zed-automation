356ee73dae9fc99424cbf7d619ed727724830586
ratanvhs <95623261+ratanvhs@users.noreply.github.com>
2022-01-20 14:36:25 +0530

    APAC-2937 / ZED-206
    ZED-206 - Breeding Horse Details Panel shown the Horse GENDER

--- tests/Breeding and Stud/BreedingAndStud.spec.ts
+++ tests/Breeding and Stud/BreedingAndStud.spec.ts
@@ -371,8 +371,19 @@ describe('Breeding And Stud', () => {
       }
     });
 
-    xit('ZED-206 - Breeding Horse Details Panel shown the Horse GENDER', async () => {
-      expect(await pages[0].isVisible(auth.objects.B_ETH_BALANCE)).toBe(true);
+    it('ZED-206 - Breeding Horse Details Panel shown the Horse GENDER', async () => {
+      await pages[0].click(breedingAndStud.objects.ddlStudSortBy)
+      await pages[0].click(breedingAndStud.objects.ddlStudSortByExpiringSoon)
+      await pages[0].waitForSelector(breedingAndStud.objects.studList.HorseList)
+      const filterHorseCount = await pages[0].innerText(breedingAndStud.objects.lblFilterCount)
+      const horseCount = filterHorseCount.substring(0,filterHorseCount.length-10).split('of')
+      for(let horseRow =1; horseRow<= parseInt(horseCount[1]); horseRow++){
+      await pages[0].click(breedingAndStud.objects.studList.collapsedPanelOpen(horseRow))
+      await pages[0].waitForTimeout(1000)
+      const genderValue = await pages[0].innerText(breedingAndStud.objects.studList.lblGenderValue(horseRow))
+      expect(['Colt', 'Stallion'].findIndex(v => v === genderValue)).not.toBe(-1)
+      await pages[0].click(breedingAndStud.objects.studList.panelMinimize(horseRow)) 
+      }
     });
 
     xit('ZED-207 - Breeding Horse Details Panel shown the Horse COAT', async () => {
