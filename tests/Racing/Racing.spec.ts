b91d4a137a42d358d097ecb9b8b4503a637c9215
davidpropfin <letuananh.it1405@gmail.com>
2022-04-11 11:48:41 +0700

    update more 79-80

--- tests/Racing/Racing.spec.ts
+++ tests/Racing/Racing.spec.ts
@@ -89,4 +89,43 @@ describe("Racing", () => {
     expect(counts.length).toBeGreaterThan(0);
   });
 
+  it('ZED-79 Racing Service allows the user to see finished race details', async () => {
+    await pages[0].click(racing.objects.menuRacing)
+    await pages[0].click(racing.objects.subMenuResults)
+    await pages[0].waitForTimeout(3000)
+    expect(await pages[0].url()).toContain('results')
+    const raceRow = await pages[0].$$(racing.objects.resultRaceTab.raceRowCount)
+    const raceCol = await pages[0].$$(racing.objects.resultRaceTab.raceColCount)
+    const eventName = await pages[0].innerText(racing.objects.resultRaceTab.eventName(1))
+    const eventType = await pages[0].innerText(racing.objects.resultRaceTab.eventType(1))
+    const distance = await pages[0].innerText(racing.objects.resultRaceTab.distance(1))
+    const date = await pages[0].innerText(racing.objects.resultRaceTab.date(1))
+    const pricePool = await pages[0].innerText(racing.objects.resultRaceTab.pricePool(1))
+    expect(await eventName).not.toBe('')
+    expect(['V', 'D', 'DG' ,'IV', 'III', 'II','I','VI'].findIndex(v => v === eventType)).not.toBe(-1)
+    expect(await distance).toContain('m')
+    expect(await date).not.toBe('')
+    expect(await raceRow.length).toBeGreaterThanOrEqual(1)
+    expect(await raceCol.length).toBe(5)
+  
+  });
+
+  it('ZED-80 Racing Service allows the user to filter `My racehorse only` while authenticated.', async () => {
+    await pages[0].click(racing.objects.menuRacing)
+    await pages[0].click(racing.objects.subMenuResults)
+    await pages[0].waitForTimeout(3000)
+    expect(await pages[0].url()).toContain('results')
+    await pages[0].click(racing.objects.resultRaceTab.myRacehorseOnlyCheckbox,{ force: true })
+    await pages[0].waitForTimeout(3000)
+    const raceRow = await pages[0].$$(racing.objects.resultRaceTab.raceRowCount)
+    const raceCol = await pages[0].$$(racing.objects.resultRaceTab.raceColCount)
+    for (let racesRow =1; racesRow<= raceRow.length; racesRow++){
+    const registeredIcon = await pages[0].locator(racing.objects.resultRaceTab.registeredIcon(racesRow)).getAttribute('class');
+    expect(await registeredIcon).toBe('registered-icon d-none d-md-inline-block')
+    }
+    expect(await raceRow.length).toBeGreaterThanOrEqual(1)
+    expect(await raceCol.length).toBe(5) 
+  });
+
+
 });
